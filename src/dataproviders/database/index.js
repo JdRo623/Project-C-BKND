59bc90e02db6f6034fea2e6f7c4b28252afd6aa1
Joan Duarte <JdRodriguez623@outlook.com>
2024-12-27 20:00:30 +0100

    DATABASE UPDATE

--- src/dataproviders/database/index.js
+++ src/dataproviders/database/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 module.exports = app => {
-    mongoose.connect('mongodb://localhost/test');
+    //mongoose.connect('mongodb://localhost/test');
+    mongoose.connect('mongodb+srv://consentdeves:zV150nvXitPEveIu@@cluster0.bm7ic.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     mongoose.connection.once('open',function(){
         console.log('Database connected Successfully');
     }).on('error',function(err){
