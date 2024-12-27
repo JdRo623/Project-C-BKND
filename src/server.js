c40f8ade74103d94c92b3d69d386ea9b45019604
Joan Duarte <JdRodriguez623@outlook.com>
2024-12-27 23:23:19 +0100

    Undefined fix 2

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const exit = require('./configurations/exit');
 const app = require('./configurations/app');
 
 const server = app.listen(/*process.env.PORT*/3000, () => {
-    logger.info(`node-essential start on port 3000${process.env.PORT}`);
+    logger.info(`node-essential start on port 3000`);
     //logger.info(`node-essential start on port 3000${process.env.PORT}`);
     exit(server);
 });
