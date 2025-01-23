22798a8cb672c12ef24d30239ac724095e83051d
Joan Duarte <JdRodriguez623@outlook.com>
2025-01-23 10:35:28 +0100

    Bug Fix: Put correct variable for email consent

--- src/entrypoints/rest/create-consent-controller.js
+++ src/entrypoints/rest/create-consent-controller.js
@@ -5,20 +5,21 @@ exports.controller = async (req, res) => {
         msg: 'Ha ocurrido un error, por favor inténtalo nuevamente.',
         errorCode: '400',
     };
+    const { id_user_s, consentCode } = req.body;
 
-    user = await findUser({consentCode: req.body.consentCode})
-    userS = await findUser({_id: req.body.id_user_s})
+    user = await findUser({consentCode: consentCode})
+    userS = await findUser({_id: id_user_s})
 
-    if(req.body.id_user_s == user._id){
+    if(id_user_s == user._id){
         response.msg = "Ha ocurrido un error, ingresa el código único de un tercero para iniciar el consentimiento."
         return res.status(response.errorCode).json(response);
     }
 
     var consentInfo = {
-        id_user_s: req.body.id_user_s,
+        id_user_s: id_user_s,
         email_user_s: userS.email,
         id_user_r: user._id,
-        email: user.email
+        email_user_r: user.email
     }
     response = await createConsentRepository(consentInfo);
 
